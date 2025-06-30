b2e58df6ba093999d7f74a31ac897e41dd98d25f
richard-javier <54337738+richard-javier@users.noreply.github.com>
2025-06-30 17:18:43 -0500

    Delete Dreamcore directory

--- Dreamcore/js/script.js
+++
@@ -1,9 +0,0 @@
-const intro = document.getElementById('introScreen');
-const main = document.getElementById('mainContent');
-const sonido = document.getElementById('ambiente');
-
-document.addEventListener('keydown', () => {
-  intro.style.display = 'none';
-  main.classList.remove('hidden');
-  sonido.play();
-});
